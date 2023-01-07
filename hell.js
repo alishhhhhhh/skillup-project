0ceba85fc68a32f15e8ed6b6f52d19c6bd24400d
alishhhhhhh <110767822+alishhhhhhh@users.noreply.github.com>
2023-01-07 15:40:02 +0545

    Add files via upload

---
+++ hell.js
@@ -0,0 +1,67 @@
+let exit = document.getElementById("exit")
+let hamb = document.getElementById("hamb");
+let cross = document.getElementById("cross")
+
+
+let hh = () => {
+    exit.style.display = "none";
+    console.log("helloo")
+
+}
+// exit.addEventListener('click', hh)
+
+let open = ()=>{
+exit.style.display = "block";
+}
+
+cross.addEventListener('click',hh)
+
+hamb.addEventListener('click', open)
+
+
+ let  = document.getElementById("sticky")
+
+// let img-con = document.getElementById("img-con")
+// let stick = document.getElementsByClassName("autoslide")
+
+// let img1 = document.getElementById("img1")
+// let click1 = document.getElementById("click1")
+
+
+// let fun1 = ()=>{
+//     img1.style.display = "block"
+
+// }
+// let img3 = document.getElementById("img3")
+// let img4 = document.getElementById("img4")
+// let img5 = document.getElementById("img5")
+// let img6= document.getElementById("img6")
+
+
+// let fun2 = ()=>{
+//     img3.style.display = "block"
+//     img1.
+
+// }
+
+// click1.addEventListener('click',fun1)
+
+
+
+// let flag = 4;
+
+
+
+
+// let slideshow = (num)=>{
+//     let slides = document.getElementsByClassName("autoslide")
+//     console.log(slides)
+//     slides[num].style.display = "block"
+    
+
+  
+
+// }
+// slideshow(flag)
+// x()
+
